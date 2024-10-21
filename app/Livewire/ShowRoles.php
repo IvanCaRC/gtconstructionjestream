257a17ea999b9367295b8f611e84b862322b842d
Ivan Axel <cupido.iacp@gmail.com>
2024-10-21 02:35:36 -0600

    agragr modal de decicion

--- app/Livewire/ShowRoles.php
+++ app/Livewire/ShowRoles.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class ShowRoles extends Component
 {
+    public $open = true;
     public function render()
     {
         return view('livewire.show-roles');
--- resources/views/livewire/show-roles.blade.php
+++ resources/views/livewire/show-roles.blade.php
@@ -1,6 +1,7 @@
 <style>
     .table thead th {
-        border-top: 0; /* Elimina el borde superior de los encabezados de la tabla */
+        border-top: 0;
+        /* Elimina el borde superior de los encabezados de la tabla */
     }
 </style>
 
@@ -15,7 +16,9 @@
                         <thead>
                             <tr>
                                 <th style="width: 35%;">Nombre del rol</th> <!-- Ajustar ancho de la columna -->
-                                <th class="d-none d-md-table-cell" wire:click="order('first_last_name')" style="cursor: pointer; width: 40%;">Descripción</th> <!-- Ajustar ancho de la columna -->
+                                <th class="d-none d-md-table-cell" wire:click="order('first_last_name')"
+                                    style="cursor: pointer; width: 40%;">Descripción</th>
+                                <!-- Ajustar ancho de la columna -->
                                 <th style="width: 12.5%;"></th>
                                 <th style="width: 12.5%;"></th>
                             </tr>
@@ -23,9 +26,11 @@
                         <tbody>
                             <tr>
                                 <td class="align-middle">Administrador</td>
-                                <td class="align-middle d-none d-md-table-cell">Se encarga de administrar el usuario</td>
+                                <td class="align-middle d-none d-md-table-cell">Se encarga de administrar el usuario
+                                </td>
                                 <td>
-                                    <button class="btn btn-info btn-sm" style="margin-left: 10px;"> <!-- Añadido margen para más espacio -->
+                                    <button class="btn btn-info btn-sm" style="margin-left: 10px;">
+                                        <!-- Añadido margen para más espacio -->
                                         <i class="fas fa-eye"></i>
                                     </button>
                                 </td>
@@ -41,4 +46,37 @@
             </div>
         </div>
     </div>
+
+
+    <x-dialog-modal wire:model="open">
+        <x-slot name='title'>
+            Editar Rol
+        </x-slot>
+        <x-slot name='content'>
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <h2>Administrador</h2>
+                        
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="description">Descripción</label>
+                    <textarea id="description" class="form-control" rows="5"></textarea> <!-- Convertido a textarea -->
+                </div>
+                
+                
+            </form>
+        </x-slot>
+        <x-slot name='footer'>
+            <button class="btn btn-secondary mr-2 disabled:opacity-50" >Cancelar</button>
+            <button class="btn btn-primary disabled:opacity-50" >Actualizar</button>
+
+
+
+
+
+        </x-slot>
+    </x-dialog-modal>
+
 </div>
