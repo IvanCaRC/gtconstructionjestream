96f32e7e859314a88cafb0d200a87f66c40136d8
DanielCG196 <danielcg76570@gmail.com>
2024-10-21 00:52:03 -0600

    Descripcion de roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 
         Storage::deleteDirectory('users');
         Storage::makeDirectory('users');
-         \App\Models\User::factory(30)->create();
+         \App\Models\User::factory(10)->create();
 
         $this->call(RoleSeeder::class);
 
