3ad3f9dd1b3b53e1351fac3f57c717d66cb9f6d0
Ivan Axel <cupido.iacp@gmail.com>
2024-12-09 06:47:02 -0600

    Primera migracion prueba

---
+++ database/migrations/2024_12_09_123143_create_familias_table.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFamiliasTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('familias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_familia')->nullable(); // Llave foránea para subfamilias
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->timestamps();
+
+            // Definir la llave foránea
+            $table->foreign('id_familia')->references('id')->on('familias')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('familias');
+    }
+}
+;
