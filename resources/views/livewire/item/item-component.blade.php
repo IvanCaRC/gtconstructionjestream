79fafea4d3fe46d6fb8951c0aa0baa3104e819a4
DanielCG196 <danielcg76570@gmail.com>
2025-01-15 10:08:34 -0600

    Listando items

--- resources/views/livewire/item/item-component.blade.php
+++ resources/views/livewire/item/item-component.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid px-4 sm:px-6 lg:px-8 py-1">
-    <h1 class="pl-4">Materiales en el inventario</h1>
+    <h1 class="pl-4">Materiales en inventario</h1>
     <div class="container-fluid px-4 sm:px-6 lg:px-8 py-1">
         <div class="card">
             <div class="card-body">
@@ -13,38 +13,47 @@
                     <div class="d-flex justify-content-between mb-3">
                         <!-- Input de búsqueda -->
                         <input type="text" class="form-control mr-2" id="searchInput" wire:model='searchTerm'
-                        wire:keydown='search' placeholder="Buscar item...">
+                            wire:keydown='search' placeholder="Buscar item...">
                     </div>
                 </div>
                 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
                 @if ($items1->count() > 0)
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <div class="categoria-header">
-                                <div><strong>Nombre</strong></div>
-                                <div><strong>Categoria</strong></div>
-                                <div><strong>Precio de proveedor</strong></div>
-                                <div><strong>Precio minorista</strong></div>
-                                <div><strong>Precio mayorista</strong></div>
-                                <div><strong>Unidad de medida</strong></div>
-                                <div><strong>Piezas para mayoreo</strong></div>
-                                <div><strong>Proveedores</strong></div>
-                                <div><strong>Descripcion</strong></div>
-                                <div><strong>Especificaciones tecnicas</strong></div>
-                                <div><strong>Recursos adicionales</strong></div>
-                                <div><strong>Tiempo de entrega</strong></div>
-                                <div><strong>Ultima modificacion</strong></div>
-
-                            </div>
-
-                            {{-- Agregar atributos para listar en la vista de proveedores --}}
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Actualizacion</th>
+                                <th>Nombre</th>
+                                <th>Categoria</th>
+                                <th>Precio de proveedor</th>
+                                <th>Precio minorista</th>
+                                <th>Precio mayorista</th>
+                                <th>Unidad de medida</th>
+                                <th>Piezas para mayoreo</th>
+                                <th>Proveedores</th>
+                                <th>Tiempo de entrega</th>
+                                <th>Ultima modificación</th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             @foreach ($items1 as $item1)
                                 <tr>
+                                    <td></td>
                                     <td class="align-middle d-none d-md-table-cell">{{ $item1->nombre }}</td>
-                                    {{-- <td class="align-middle d-none d-md-table-cell">{{ $item1->updated_at }}</td> --}}
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td class="align-middle d-none d-md-table-cell">{{ $item1->updated_at }}</td>
                             @endforeach
-                        </li>
-                    </ul>
+                        </tbody>
+                    </table>
                 @else
                     <div class='px-6 py-2'>
                         <p>No hay resultados</p>
