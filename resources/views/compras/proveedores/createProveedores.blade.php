5558fb128082f446f66f399703257d4b3ec5c666
Ivan Axel <cupido.iacp@gmail.com>
2025-01-19 10:58:31 -0600

    Apartir de aqui

--- resources/views/compras/proveedores/createProveedores.blade.php
+++ resources/views/compras/proveedores/createProveedores.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
 <!-- Incluir Leaflet CSS -->
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha384-hs2+e1ecAhXZrp0TBECMsTh0FeP0K2j3/Zni+1iZQODhFboc2vm4IpYgrbs4zXrC" crossorigin=""/>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha384-hs2+e1ecAhXZrp0TBECMsTh0FeP0K2j3/Zni+1iZQODhFboc2vm4IpYgrbs4zXrC" crossorigin="" />
 
 <!-- Incluir Leaflet JS -->
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha384-M0jX1lz+V5Ftchz4HJaP3vTTv1od8Hcewgsq/oa9OYypsA8SjlAZ5d+8xvQZZlAA" crossorigin=""></script>
-
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha384-M0jX1lz+V5Ftchz4HJaP3vTTv1od8Hcewgsq/oa9OYypsA8SjlAZ5d+8xvQZZlAA" crossorigin=""></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 <style>
     .file-upload {
         display: flex;
@@ -41,12 +44,10 @@
         text-align: center;
         line-height: 36px;
     }
-    #map{
-        margin: 0 auto;
-        width: 20%;
-        min-height: 250px;
-        max-height: 250px;
-        height: 100%;
+
+    #map {
+
+        height: 180px;
     }
 </style>
 @section('title', 'Crear proveedor')
@@ -55,5 +56,5 @@
 @section('activeProveedores', 'active')
 @section('activeFondoPermanenteProveedores', 'background-permanent')
 @section('contend')
-@livewire('proveedor.create-proveedor')
+    @livewire('proveedor.create-proveedor')
 @endsection
--- resources/views/livewire/proveedor/create-proveedor.blade.php
+++ resources/views/livewire/proveedor/create-proveedor.blade.php
@@ -4,132 +4,129 @@
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <div class="card">
             <div class="card-body">
-                <div id="map"></div>
-                <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-                    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-                <script>
-                    var map = L.map('map').setView([51.505, -0.09], 13);
-                    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        maxZoom: 19,
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    }).addTo(map);
-                </script>
-                <form>
-                    <div class="form-group">
-                        <label for="nombre">Nombre</label>
-                        <input type="text" id="nombre" class="form-control"wire:model.defer="nombre">
-                    </div>
 
-                    <div class="form-group">
-                        <label for="descripcion">Descripción</label>
-                        <textarea rows="5" id="descripcion" class="form-control" wire:model="descripcion"></textarea>
-                    </div>
+                <div class="form-group">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" id="nombre" class="form-control"wire:model.defer="nombre">
+                </div>
 
-                    <div class="form-group">
-                        <label for="descripcion">Correo</label>
-                        <input type="email" id="email" class="form-control" wire:model="correo"></input>
-                    </div>
+                <div class="form-group">
+                    <label for="descripcion">Descripción</label>
+                    <textarea rows="5" id="descripcion" class="form-control" wire:model="descripcion"></textarea>
+                </div>
 
-                    <div class="form-group">
-                        <label for="descripcion">RFC</label>
-                        <input id="descripcion" class="form-control" wire:model="rfc"></input>
-                    </div>
-                    <div class="form-group">
-                        <label>Teléfonos</label>
-                        @foreach ($telefonos as $index => $telefono)
-                            <div class="input-group mb-2">
-                                <input type="text" class="form-control"
-                                    wire:model.defer="telefonos.{{ $index }}" placeholder="Teléfono">
-                                @if ($index > 0)
-                                    <div class="input-group-append">
-                                        <button type="button" class="btn btn-danger ml-2"
-                                            wire:click="removeTelefono({{ $index }})">Eliminar</button>
-                                    </div>
-                                @endif
-                            </div>
-                        @endforeach
-                        <button type="button" class="btn btn-primary mt-2" wire:click="addTelefono">Agregar otro
-                            teléfono</button>
-                    </div>
-                    <div class="form-group">
-                        <label>Familias</label>
+                <div class="form-group">
+                    <label for="descripcion">Correo</label>
+                    <input type="email" id="email" class="form-control" wire:model="correo"></input>
+                </div>
+
+                <div class="form-group">
+                    <label for="descripcion">RFC</label>
+                    <input id="descripcion" class="form-control" wire:model="rfc"></input>
+                </div>
+                <div class="form-group">
+                    <label>Teléfonos</label>
+                    @foreach ($telefonos as $index => $telefono)
                         <div class="input-group mb-2">
-                            @if (count($familiasSeleccionadas) > 0)
-                                @foreach ($familiasSeleccionadas as $index => $familia)
-                                    <div class="w-100 d-flex align-items-center mb-2">
-                                        <div class="flex-grow-1">
-                                            {{ $familia->nombre }}
-                                        </div>
-                                        <button type="button" wire:click="removeFamilia({{ $index }})"
-                                            class="btn btn-danger btn-sm ml-2">Eliminar</button>
-                                    </div>
-                                @endforeach
-                            @else
-                                <div class="no-familias-seleccionadas w-100">
-                                    No hay familias seleccionadas
+                            <input type="text" class="form-control" wire:model.defer="telefonos.{{ $index }}"
+                                placeholder="Teléfono">
+                            @if ($index > 0)
+                                <div class="input-group-append">
+                                    <button type="button" class="btn btn-danger ml-2"
+                                        wire:click="removeTelefono({{ $index }})">Eliminar</button>
                                 </div>
                             @endif
                         </div>
-                        <a href="#" wire:click="$set('openModalFamilias', true)"
-                            class="btn btn-primary mt-3">Agregar Familia</a>
-                    </div>
-                    <div class="form-group">
-                        <label>Direcciones</label>
-                        <div class="input-group mb-2">
-                            No hay direcciones asignadas
+                    @endforeach
+                    <button type="button" class="btn btn-primary mt-2" wire:click="addTelefono">Agregar otro
+                        teléfono</button>
+                </div>
+                <div class="form-group"> <label for="archivosFacturacion">Archivos de facturación</label>
+                    @if (!$facturacion)
+                        <div class="file-upload" onclick="document.getElementById('archivosFacturacion').click();">
+                            <span class="file-upload-icon">&#x1F4C2;</span> <span class="file-upload-text">Buscar
+                                archivos<br>Arrastre y suelte archivos aquí</span> <input type="file"
+                                id="archivosFacturacion" class="form-control-file" wire:model="facturacion"
+                                accept=".pdf">
+                        </div> <small class="form-text text-muted">Por favor, suba archivos en
+                            formato PDF solamente.</small>
+                    @else
+                        <div class="form-group">
+                            <div class="file-upload"
+                                onclick="document.getElementById('archivosFacturacionCar').click();">
+                                <span class="file-upload-icon">&#x1F4C4;</span>
+                                <span class="file-upload-text">Archivo Cargado<br>{{ $fileNameFacturacion }}</span>
+                                <input type="file" id="archivosFacturacionCar" class="form-control-file"
+                                    wire:model="facturacion" accept=".pdf">
+                            </div>
+                            <small class="form-text text-muted">Por favor, suba archivos en formato
+                                PDF solamente.</small>
                         </div>
-                        <a href="#" wire:click="$set('openModalDireccion', true)"
-                            class="btn btn-primary mt-3">Agregar Direccion</a>
-                    </div>
-                    <div class="form-group"> <label for="archivosFacturacion">Archivos de facturación</label>
-                        @if (!$facturacion)
-                            <div class="file-upload" onclick="document.getElementById('archivosFacturacion').click();">
-                                <span class="file-upload-icon">&#x1F4C2;</span> <span class="file-upload-text">Buscar
-                                    archivos<br>Arrastre y suelte archivos aquí</span> <input type="file"
-                                    id="archivosFacturacion" class="form-control-file" wire:model="facturacion"
-                                    accept=".pdf">
-                            </div> <small class="form-text text-muted">Por favor, suba archivos en
-                                formato PDF solamente.</small>
-                        @else
-                            <div class="form-group">
-                                <div class="file-upload"
-                                    onclick="document.getElementById('archivosFacturacionCar').click();">
-                                    <span class="file-upload-icon">&#x1F4C4;</span>
-                                    <span class="file-upload-text">Archivo Cargado<br>{{ $fileNameFacturacion }}</span>
-                                    <input type="file" id="archivosFacturacionCar" class="form-control-file"
-                                        wire:model="facturacion" accept=".pdf">
+                    @endif
+                </div>
+
+                <div class="form-group"> <label for="archivosFacturacion">Datos Bancarios</label>
+                    @if (!$bancarios)
+                        <div class="file-upload" onclick="document.getElementById('archivosBancarios').click();">
+                            <span class="file-upload-icon">&#x1F4C2;</span> <span class="file-upload-text">Buscar
+                                archivos<br>Arrastre y suelte archivos aquí</span> <input type="file"
+                                id="archivosBancarios" class="form-control-file" wire:model="bancarios" accept=".pdf">
+                        </div> <small class="form-text text-muted">Por favor, suba archivos en
+                            formato PDF solamente.</small>
+                    @else
+                        <div class="form-group">
+                            <div class="file-upload" onclick="document.getElementById('archivosBancariosCar').click();">
+                                <span class="file-upload-icon">&#x1F4C4;</span>
+                                <span class="file-upload-text">Archivo Cargado<br>{{ $fileNameBancarios }}</span>
+                                <input type="file" id="archivosBancariosCar" class="form-control-file"
+                                    wire:model="bancarios" accept=".pdf">
+                            </div>
+                            <small class="form-text text-muted">Por favor, suba archivos en formato
+                                PDFsolamente.</small>
+                        </div>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <label>Familias</label>
+                    <div class="input-group mb-2">
+                        @if (count($familiasSeleccionadas) > 0)
+                            @foreach ($familiasSeleccionadas as $index => $familia)
+                                <div class="w-100 d-flex align-items-center mb-2">
+                                    <div class="flex-grow-1">
+                                        {{ $familia->nombre }}
+                                    </div>
+                                    <button type="button" wire:click="removeFamilia({{ $index }})"
+                                        class="btn btn-danger btn-sm ml-2">Eliminar</button>
                                 </div>
-                                <small class="form-text text-muted">Por favor, suba archivos en formato
-                                    PDF solamente.</small>
+                            @endforeach
+                        @else
+                            <div class="no-familias-seleccionadas w-100">
+                                No hay familias seleccionadas
                             </div>
                         @endif
                     </div>
-
-                    <div class="form-group"> <label for="archivosFacturacion">Datos Bancarios</label>
-                        @if (!$bancarios)
-                            <div class="file-upload" onclick="document.getElementById('archivosBancarios').click();">
-                                <span class="file-upload-icon">&#x1F4C2;</span> <span class="file-upload-text">Buscar
-                                    archivos<br>Arrastre y suelte archivos aquí</span> <input type="file"
-                                    id="archivosBancarios" class="form-control-file" wire:model="bancarios"
-                                    accept=".pdf">
-                            </div> <small class="form-text text-muted">Por favor, suba archivos en
-                                formato PDF solamente.</small>
-                        @else
-                            <div class="form-group">
-                                <div class="file-upload"
-                                    onclick="document.getElementById('archivosBancariosCar').click();">
-                                    <span class="file-upload-icon">&#x1F4C4;</span>
-                                    <span class="file-upload-text">Archivo Cargado<br>{{ $fileNameBancarios }}</span>
-                                    <input type="file" id="archivosBancariosCar" class="form-control-file"
-                                        wire:model="bancarios" accept=".pdf">
-                                </div>
-                                <small class="form-text text-muted">Por favor, suba archivos en formato
-                                    PDFsolamente.</small>
-                            </div>
-                        @endif
+                    <button href="#" wire:click="$set('openModalFamilias', true)"
+                        class="btn btn-primary mt-3">Agregar Familia</button>
+                </div>
+                <div class="form-group">
+                    <label>Direcciones</label>
+                    <div class="input-group mb-2">
+                        No hay direcciones asignadas
                     </div>
+                    <button wire:click="$set('openModalDireccion', false)" class="btn btn-primary mt-3">Cerrar
+                        Direccion</a>
+                        {{-- El boton cambia el estado de openModalDirecion --}}
+                        <button wire:click="$set('openModalDireccion', true)" class="btn btn-primary mt-3">Abrir
+                            Direccion</a>
+                            {{-- El boton cambia el estado de openModalDirecion --}}
+                </div>
 
-                </form>
+                @if ($openModalDireccion)
+                    {{-- Aqui se agrga el mapa se openModalDirecion es TRUE --}}
+                    <div id="map"></div>
+                @endif
+
                 <button type="button" onclick="confirmSave()" class="btn btn-primary mt-3">Crear proveedor</button>
             </div>
         </div>
@@ -166,35 +163,17 @@
                 wire:loading.attr="disabled">Cancelar</button>
             <button class="btn btn-primary disabled:opacity-50" wire:loading.attr="disabled"
                 wire:click="confirmFamilia">Agregar familia</button>
-
         </x-slot>
     </x-dialog-modal>
-
-    <x-dialog-modal wire:model="openModalDireccion">
-        <x-slot name='title'>
-            Añadir Dirección
-        </x-slot>
-        <x-slot name='content'>
-            <form>
-                <div id="map"></div>
-                <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-                    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-                <script>
-                    var map = L.map('map').setView([51.505, -0.09], 13);
-                    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        maxZoom: 19,
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    }).addTo(map);
-                </script>
-            </form>
-        </x-slot>
-        <x-slot name='footer'>
-            <button class="btn btn-secondary mr-2 disabled:opacity-50" wire:click="$set('openModalDireccion',false)"
-                wire:loading.attr="disabled">Cancelar</button>
-            <button class="btn btn-primary disabled:opacity-50" wire:loading.attr="disabled"
-                wire:click="confirmDireccion">Agregar Dirección</button>
-        </x-slot>
-    </x-dialog-modal>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script>
+        var map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    </script>
 
     <script>
         function confirmSave() {
