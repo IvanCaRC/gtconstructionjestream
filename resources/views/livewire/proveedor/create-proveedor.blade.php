4200227b8120428098a705926187165d54f0b25a
Ivan Axel <cupido.iacp@gmail.com>
2025-01-20 07:42:40 -0600

    Mapa visible mostrara

--- resources/views/livewire/proveedor/create-proveedor.blade.php
+++ resources/views/livewire/proveedor/create-proveedor.blade.php
@@ -134,7 +134,7 @@
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <script>
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        var map = L.map('map').setView([51.505, -0.09], 14);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
