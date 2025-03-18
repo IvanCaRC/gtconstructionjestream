e3872e9b3ae9d2802fcc0fa128a662058347af27
DanielCG196 <danielcg76570@gmail.com>
2025-03-18 11:21:59 -0600

    x

--- resources/views/livewire/cliente/card-cliente.blade.php
+++ resources/views/livewire/cliente/card-cliente.blade.php
@@ -6,23 +6,21 @@
                 <button type="button" class="btn-icon">
                     <i class="fas fa-arrow-left"></i>
                 </button>
-                <h3 class="ml-3">Datos de cliente</h3>
+                <h3 class="ml-3">Datos del cliente</h3>
             </div>
 
             <div class="col-md-2">
-                <a href="#" wire:click=""
-                    class="d-block mb-3" wire:click="">Editar cliente</a>
+                <a href="#" wire:click="" class="btn btn-primary d-block mb-3" wire:click="">Editar cliente</a>
             </div>
         </div>
-
     </div>
     <div class="card-body">
         <div>
-            <h2>{{ $clienteEspecifico->nombre }}</h2>
+            <h2 class="resaltado">{{ $clienteEspecifico->nombre }}</h2>
         </div>
         <div class="row">
             <div class="col-md-2 ">
-                <h4>Datos generales</h4>
+                <h4 style="font-weight: bold;">Datos generales</h4>
                 <div>
                     <Span>Correo: </Span>
                     <label>{{ $clienteEspecifico->correo }}</label>
@@ -32,7 +30,7 @@
                     <label>{{ $clienteEspecifico->rfc }}</label>
                 </div>
                 <div>
-                    <Span>Proyectos: </Span>
+                    <Span>Proyectos registrados: </Span>
                     <label>{{ $clienteEspecifico->proyectos }}</label>
                 </div>
                 <div>
@@ -46,7 +44,7 @@
             </div>
             <div class="col-md-2 ">
                 @if ($telefonos)
-                    <h4>Telefonos</h4>
+                    <h4 style="font-weight: bold;">Telefonos</h4>
                     @foreach ($telefonos as $index => $telefono)
                         <div>
                             <label class="enunciado-label">
@@ -60,7 +58,7 @@
                 @endif
             </div>
             <div class="col-md-5 ">
-                <h4>Cuentas Bancarias</h4>
+                <h4 style="font-weight: bold;">Cuentas bancarias</h4>
                 @if ($bancarios)
                     <table class="table">
                         <thead>
@@ -93,7 +91,7 @@
                 @endif
             </div>
             <div class="col-md-3 ">
-                <h4>Direcciones</h4>
+                <h4 style="font-weight: bold;">Direcciones</h4>
                 @if ($clienteEspecifico->direcciones->count() > 0)
                     @foreach ($clienteEspecifico->direcciones as $direccion)
                         {{ $direccion->estado }}, {{ $direccion->ciudad }}...<br>
@@ -103,5 +101,16 @@
                 @endif
             </div>
         </div>
+
+        <style>
+            .resaltado {
+                color: #4682b4; /* Color del texto */
+                font-weight: bold; /* Texto en negrita */
+                padding: 5px; /* Espaciado interno */
+                border-radius: 5px; /* Bordes redondeados */
+                text-align: left; /* Centrar el texto (opcional) */
+            }
+        </style>
+
     </div>
 </div>
