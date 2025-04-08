54b7c371851f12f529155602c0440635e252aabe
DanielCG196 <danielcg76570@gmail.com>
2025-04-08 02:54:05 -0600

    update cliente mensaje

--- resources/views/livewire/cliente/edit-cliente.blade.php
+++ resources/views/livewire/cliente/edit-cliente.blade.php
@@ -89,13 +89,13 @@
             @this.call('updateCliente').then(response => {
                 if (response.cliente_id) {
                     // Guardar el ID del cliente recién creado en un campo oculto
-                    document.getElementById('cliente-id-input').value = response.cliente_id;
+                    // document.getElementById('cliente-id-input').value = response.cliente_id;
 
                     // Convertir las direcciones a formato JSON
-                    const directionsJSON = JSON.stringify(savedAddresses);
+                    // const directionsJSON = JSON.stringify(savedAddresses);
 
                     // Asignar el valor al campo oculto
-                    document.getElementById('direcciones-input').value = directionsJSON;
+                    // document.getElementById('direcciones-input').value = directionsJSON;
 
                     // Mostrar la alerta después de la creación si todo es correcto
                     Swal.fire({
