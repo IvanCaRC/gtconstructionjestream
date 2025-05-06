1374a6575e9961d9fa2db98f74ba0e526640e758
DanielCG196 <danielcg76570@gmail.com>
2025-05-06 05:32:40 -0600

    estructura lista a cotizar 1.0

--- resources/views/pdf/lista.blade.php
+++ resources/views/pdf/lista.blade.php
@@ -7,73 +7,181 @@
     <style>
         body {
             font-family: DejaVu Sans, sans-serif;
+            font-size: 12px;
         }
 
         h1 {
             text-align: center;
             color: #333;
+            margin-bottom: 20px;
+        }
+
+        .section {
+            border-bottom: 2px solid #333;
+            margin-top: 20px;
+            padding-bottom: 5px;
+            font-size: 14px;
+            font-weight: bold;
+            text-transform: uppercase;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 10px;
+        }
+
+        th,
+        td {
+            border: 1px solid #ccc;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f4f4f4;
+        }
+
+        .image-item {
+            text-align: center;
+        }
+
+        img {
+            max-width: 100px;
         }
     </style>
 </head>
 
 <body>
     <h1>{{ $title }}</h1>
-    <p><strong>Proyecto:</strong> {{ $proyecto }}</p>
-    <p><strong>Tipo:</strong> {{ $proyecto_tipo }}</p>
-    <p><strong>Fecha:</strong> {{ $proyecto_fecha }}</p>
-    <p><strong>Atentido por:</strong> {{ $usuario }} {{ $usuario_first_last_name }} {{ $usuario_second_last_name }}
-    </p>
-    <p><strong>Cliente:</strong> {{ $cliente_nombre }}</p>
-    <p><strong>Correo:</strong> {{ $cliente_correo }}</p>
-    <p><strong>Dirección:</strong> {{ $cliente_direccion }}</p>
-    <p><strong>Contacto Principal:</strong> {{ $cliente_contacto_1 }} - {{ $cliente_telefono_1 }}</p>
-    <p><strong>Contacto adicional:</strong> {{ $cliente_contacto_2 }} - {{ $cliente_telefono_2 }}</p>
-    <p><strong>Frente:</strong> {{ $frentes }}</p>
-    <p><strong>Fondo:</strong> {{ $fondos }}</p>
-    <p><strong>Altura del Techo:</strong> {{ $alturasTecho }}</p>
-    <p><strong>Área Total:</strong> {{ $areasTotales }}</p>
-    <p><strong>Altura de los Muros:</strong> {{ $alturasMuros }}</p>
-    <p><strong>Canalón:</strong> {{ $canalones }}</p>
-    <p><strong>Perimetral:</strong> {{ $perimetrales }}</p>
-    <p><strong>Caballete:</strong> {{ $caballetes }}</p>
-    {{-- <p><strong>Elementos:</strong> {{ $estructuras }}</p>
-    <p><strong>Cantidades:</strong> {{ $cantidades }}</p> --}}
-    <p><strong>Elementos Adicionales/Cantidad:</strong> {{ $estructura_cantidad }}</p>
-    {{-- <p><strong>Ítems a Cotizar:</strong> {{ $items_cotizar }}</p> --}}
-    @foreach ($items_cotizar_data as $item)
-        @php
-            // Extraer solo la parte relevante de la ruta
-            $path = storage_path('app/public/' . str_replace('http://127.0.0.1:8000/storage/', '', $item['imagen']));
 
-            // Verificamos que el archivo realmente existe antes de procesarlo
-            $base64Item = null;
-            if (file_exists($path)) {
-                $type = pathinfo($path, PATHINFO_EXTENSION);
-                $data = file_get_contents($path);
-                $base64Item = 'data:image/' . $type . ';base64,' . base64_encode($data);
-            }
-        @endphp
+    <p class="section">Información del Proyecto</p>
+    <table>
+        <tr>
+            <th>Proyecto</th>
+            <td>{{ $proyecto }}</td>
+        </tr>
+        <tr>
+            <th>Tipo</th>
+            <td>{{ $proyecto_tipo }}</td>
+        </tr>
+        <tr>
+            <th>Fecha</th>
+            <td>{{ $proyecto_fecha }}</td>
+        </tr>
+        <tr>
+            <th>Atendido por</th>
+            <td>{{ $usuario }} {{ $usuario_first_last_name }} {{ $usuario_second_last_name }}</td>
+        </tr>
+    </table>
 
-        {{-- <p><strong>Ruta generada:</strong> {{ $item['imagen'] }}</p>
-        <p><strong>Ruta procesada para Base64:</strong> {{ $path }}</p> --}}
+    <p class="section">Datos del Cliente</p>
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <td>{{ $cliente_nombre }}</td>
+        </tr>
+        <tr>
+            <th>Correo</th>
+            <td>{{ $cliente_correo }}</td>
+        </tr>
+        <tr>
+            <th>Dirección</th>
+            <td>{{ $cliente_direccion }}</td>
+        </tr>
+        <tr>
+            <th>Contacto Principal</th>
+            <td>{{ $cliente_contacto_1 }} - {{ $cliente_telefono_1 }}</td>
+        </tr>
+        <tr>
+            <th>Contacto Adicional</th>
+            <td>{{ $cliente_contacto_2 }} - {{ $cliente_telefono_2 }}</td>
+        </tr>
+    </table>
 
-        @if ($base64Item)
-            <p><strong>Imagen del Ítem:</strong></p>
-            <img src="{{ $base64Item }}">
-        @else
-            <p>⚠️ Imagen no disponible</p>
+    <p class="section">Medidas del Proyecto</p>
+    <table>
+        <tr>
+            <th>Frente</th>
+            <td>{{ $frentes }}</td>
+        </tr>
+        <tr>
+            <th>Fondo</th>
+            <td>{{ $fondos }}</td>
+        </tr>
+        <tr>
+            <th>Altura del Techo</th>
+            <td>{{ $alturasTecho }}</td>
+        </tr>
+        <tr>
+            <th>Área Total</th>
+            <td>{{ $areasTotales }}</td>
+        </tr>
+        <tr>
+            <th>Altura de los Muros</th>
+            <td>{{ $alturasMuros }}</td>
+        </tr>
+        <tr>
+            <th>Canalón</th>
+            <td>{{ $canalones }}</td>
+        </tr>
+        <tr>
+            <th>Perimetral</th>
+            <td>{{ $perimetrales }}</td>
+        </tr>
+        <tr>
+            <th>Caballete</th>
+            <td>{{ $caballetes }}</td>
+        </tr>
+    </table>
+
+    <p class="section">Elementos Adicionales y Cantidades</p>
+    <table>
+        <tr>
+            <th>Descripción</th>
+            <th>Cantidad</th>
+        </tr>
+        @if (!empty($estructura_cantidad) && is_array($estructura_cantidad))
+            <table>
+                <tr>
+                    <th>Descripción</th>
+                    <th>Cantidad</th>
+                </tr>
+                @foreach ($estructura_cantidad as $item)
+                    <tr>
+                        <td>{{ $item['estructura'] }}</td>
+                        <td>{{ $item['cantidad'] }}</td>
+                    </tr>
+                @endforeach
+            </table>
         @endif
+    </table>
 
-        <p><strong>Nombre:</strong> {{ $item['nombre'] }}</p>
-        <p><strong>Marca:</strong> {{ $item['marca'] }}</p>
-        <p><strong>Cantidad Solicitada:</strong> {{ $item['cantidad'] }}</p>
-        <hr>
-    @endforeach
-    {{-- <p><strong>Ítems Temporales:</strong> {{ $items_cotizar_temporales }}</p> --}}
-    @foreach ($items_cotizar_temporales_data as $item)
-        <p><strong>Nombre:</strong> {{ $item['nombre'] }}</p>
-        <p><strong>Descripción:</strong> {{ $item['descripcion'] }}</p>
-        <p><strong>Unidad:</strong> {{ $item['unidad'] }}</p>
+    <p class="section">Ítems a Cotizar</p>
+    @foreach ($items_cotizar_data as $item)
+        <table>
+            <tr class="image-item">
+                <td colspan="2">
+                    @if ($item['imagen'])
+                        <img src="{{ $item['imagen'] }}">
+                    @else
+                        <p>⚠️ Imagen no disponible</p>
+                    @endif
+                </td>
+            </tr>
+            <tr>
+                <th>Nombre</th>
+                <td>{{ $item['nombre'] }}</td>
+            </tr>
+            <tr>
+                <th>Marca</th>
+                <td>{{ $item['marca'] }}</td>
+            </tr>
+            <tr>
+                <th>Cantidad Solicitada</th>
+                <td>{{ $item['cantidad'] }}</td>
+            </tr>
+        </table>
         <hr>
     @endforeach
 </body>
